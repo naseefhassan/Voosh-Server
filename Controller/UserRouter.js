4268c7a6d7701363ba59f6b7875a0dabcb9a03fe
Naseef <naseefhassanmgmail.com>
2024-07-20 12:59:11 +0530

    delete feature added

--- Controller/UserRouter.js
+++ Controller/UserRouter.js
@@ -27,6 +27,26 @@ const object = {
 
     }
   },
+  deleteTask:async(req,res)=>{
+    try {
+      const taskId = req.params.taskId
+      await TaskSchema.findByIdAndDelete(taskId)
+      res.status(200).json({ message: "task deletion success"});
+    } catch (error) {
+      console.error(error);
+      res.status(400).json({ message: "task deletion failed"});
+    }
+  },
+  editTask:async(req,res)=>{
+    try {
+      const taskId = req.params.taskId
+     const editedTask = await TaskSchema.findByIdAndUpdate(taskId)
+      res.status(200).json({ message: "editing task success", editedTask});
+    } catch (error) {
+      console.error(error);
+      res.status(400).json({ message: "editing task failed"});
+    }
+  }
 };
 
 module.exports = object;
--- Router/UserRouter.js
+++ Router/UserRouter.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const {addTask, getTask} = require("../Controller/UserRouter");
+const {addTask, getTask, deleteTask, editTask} = require("../Controller/UserRouter");
 
 
 router.post('/task',addTask)
 router.get('/getTask',getTask)
+router.delete('/deleteTask/:taskId',deleteTask)
+router.put('/editTask/:taskId',editTask)
 module.exports = router;
