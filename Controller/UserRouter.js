a2b78521d9967fe6dda1b61781ec2a57f74eeb38
Naseef <naseefhassanmgmail.com>
2024-07-20 14:35:14 +0530

    view details finished

--- Controller/UserRouter.js
+++ Controller/UserRouter.js
@@ -38,13 +38,34 @@ const object = {
     }
   },
   editTask:async(req,res)=>{
+    console.log('hlo');
     try {
       const taskId = req.params.taskId
-     const editedTask = await TaskSchema.findByIdAndUpdate(taskId)
+      const {task, description} = req.body
+      console.log(req.body);
+     const editedTask = await TaskSchema.findByIdAndUpdate(taskId,{
+      task:task,
+      description:description
+     },{new:true})
       res.status(200).json({ message: "editing task success", editedTask});
     } catch (error) {
       console.error(error);
       res.status(400).json({ message: "editing task failed"});
+    }
+  },
+  value:async(req,res)=>{
+    try {
+      const taskId = req.params.taskId
+      const editValue = await TaskSchema.findById(taskId)
+      if(!editValue){
+        return res.status(404).json({ message: "Task not found" });
+
+      }
+      res.status(200).json({ message: "edit value fetching success", editValue});
+
+    } catch (error) {
+      console.error(error);
+      res.status(400).json({ message: "edit value fetching failed"});
     }
   }
 };
--- Router/UserRouter.js
+++ Router/UserRouter.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {addTask, getTask, deleteTask, editTask} = require("../Controller/UserRouter");
+const {addTask, getTask, deleteTask, editTask, value} = require("../Controller/UserRouter");
 
 
 router.post('/task',addTask)
 router.get('/getTask',getTask)
 router.delete('/deleteTask/:taskId',deleteTask)
 router.put('/editTask/:taskId',editTask)
+router.get('/value/:taskId',value)
+
 module.exports = router;
